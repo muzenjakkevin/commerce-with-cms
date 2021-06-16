968c2b9975199821a7906738136c2396d7c14f92
muzenjakkevin <muzenjak57@gmail.com>
2021-06-16 12:47:47 +0200

    added few changes

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Home Page</h1>
+      <h1>Homepage works</h1>
     </div>
   )
 }
